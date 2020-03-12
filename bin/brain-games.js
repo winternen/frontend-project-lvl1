bd39f281b946a37c91dd0d857dc17aec54ba8c91
winternen <winterraisen@protonmail.com>
2020-03-12 18:05:14 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
+import printGreeting from '../src/index.js';
 
 console.log('Welcome to the Brain Games!');
+printGreeting();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const printGreeting = () => {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
+};
+
+export default printGreeting;
