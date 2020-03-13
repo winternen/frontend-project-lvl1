acace1c3292343c566cdbc26c95d9862ce96f2ff
winternen <winterraisen@protonmail.com>
2020-03-13 15:45:06 +0300

    add ESLint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const printGreeting = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-    return name;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default printGreeting;
